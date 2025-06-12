040f288a660296fd4e2b36afaf80fd4ef8a26646
Osvaldo Cruz <osvaldobone16@gmail.com>
2025-06-12 12:24:19 -0300

    #Alt02

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,7 @@
-self.addEventListener("install", (e) => {
-  console.log("Service Worker instalado.");
+self.addEventListener("install", (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(fetch(event.request).catch(() => caches.match("index.html")));
 });
